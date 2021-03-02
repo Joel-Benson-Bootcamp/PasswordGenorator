15049161c849b3f13462403ebc3eadb8828a19a6
Joel Benson <bensonjoel411@gmail.com>
2021-03-02 09:52:16 -0500

    Added var for password length/prompt and added password input criteria

--- password homework/Develop/script.js
+++ password homework/Develop/script.js
@@ -1,24 +1,39 @@
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordPrompt = prompt("Choose a password length between 8-128 characters");
+var passwordLength = parseInt(passwordPrompt, 10);
+
+console.log(passwordLength, "password length"); 
+console.log(passwordPrompt, "password prompt"); 
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  
+  var charset = []; 
+  if(lowercase == true) {
+    var lowercaseChar="abcdefghijklmnopqrstuvwxyz"; 
+    charset.push(lowercaseChar);  
+  } 
+  if (uppercase == true) {
+    var uppercaseChar="ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+    charset.push(uppercaseChar); 
+  } 
+  if (numeric == true) { 
+    var numericChar="0123456789"; 
+    charset.push(numericChar); 
+  } 
+  if (special == true) { 
+    var specialChar="!@#$%^&*?"; 
+    charset.push(specialChar); 
+  }
+
+  var password = generatePassword(charset); 
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+
 }
-function getUpper(){
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-function getLower(){
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-function getNumber(){
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-function getRandomSymbol(){
-  var symbol = "!@#$%^&*(){}[]=<>/,.|~?";
-  return symbol[Math.floor(Math.random()*symbol.length)];
-}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
